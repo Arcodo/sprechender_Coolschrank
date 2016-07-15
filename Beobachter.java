e0ef323952672bd5f38bc3734c35214da3ebbd59
unknown <11b23@.JMF.GYM.BUL>
2016-07-15 09:20:17 +0200

    neue Dateien

---
+++ Beobachter.java
@@ -0,0 +1,9 @@
+package sprechender_Coolschrank;
+/**
+ * Das Interface Beobachter garantiert die Existenz der Methode update(Model model);
+  */
+public interface Beobachter
+{
+   public void update(Model model);
+   int i = 0; //Mist!!!!!!
+}
---
+++ Produkt.java
@@ -0,0 +1,29 @@
+package sprechender_Coolschrank;
+public class Produkt {
+	private int maxAnzahl;
+	private String name;
+	private int menge;
+	
+	public Produkt(int maxAnzahlNeu, String neuerName, int neueMenge)
+	{
+		maxAnzahl = maxAnzahlNeu;
+		name = neuerName;
+		menge = neueMenge;
+	}
+	
+	public String nameAusgeben()
+	{
+		return name;
+	}
+	
+	public int mengeAusgeben()
+	{
+		return menge;
+	}
+	
+	public int einkaufsMengeBerechnen()
+	{
+		return maxAnzahl - menge;
+	}
+	
+}
